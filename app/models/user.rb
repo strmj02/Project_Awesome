eea5ce59037afa6a0c44803f4bc6e3558284d33d
AceIsAwesome <stmjv02@moravian.edu>
2013-03-26 14:49:37 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :password, :profile
+  require 'digest/md5'
+
+  attr_accessible :name, :password, :profile, :email, :password_confirmation
+  
+  validates :name, :presence => TRUE, :length => { :minimum => 2, :allow_blank => TRUE }
+  
+  
 end
