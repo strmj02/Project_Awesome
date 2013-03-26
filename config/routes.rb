fa0cbada454ca1fdc855fb8ef33f151bcff154df
AceIsAwesome <stmjv02@moravian.edu>
2013-03-26 14:20:32 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RecipeGenerator::Application.routes.draw do
   resources:users
-
+  match '/register' => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +50,7 @@ RecipeGenerator::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
