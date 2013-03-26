b146db285c892df489da1cf5f8644900f66e1448
AceIsAwesome <stmjv02@moravian.edu>
2013-03-26 14:58:58 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ActiveRecord::Base
   require 'digest/md5'
 
   attr_accessible :name, :password, :profile, :email, :password_confirmation
+  before_save :encrypt_password
   
   validates :name, :presence => TRUE, :length => { :minimum => 2, :allow_blank => TRUE }
   
+  validates :password, :presence => TRUE, :length => { :minimum => 6, :allow_blank => TRUE }, :confirmation => TRUE
   
+  validates :password_confirmation, :presence => TRUE
+  
+  validates :email, :presence => TRUE, :uniqueness => TRUE
+  
+  def encrypt_password
+    self.password = Digest::MD5.hexdigest(password)
+  end
 end
